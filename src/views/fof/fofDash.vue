8553147c146d78687831acb922a8949d95e28dff
joyfun <joyfun@joyfun.net>
2022-02-18 23:29:29 +0800

    add delete

--- src/views/fof/fofDash.vue
+++ src/views/fof/fofDash.vue
@@ -14,11 +14,10 @@
         </vxe-table> -->
 
 <el-row>
-  <el-col span="14">
+  <el-col :span="14">
         <vxe-table
           border
           :align="allAlign"
-          style="width:800px"
           size="mini"
           :mouse-config="{selected: true}"
           show-overflow
@@ -26,7 +25,7 @@
           :edit-config="{trigger: 'dblclick', mode: 'cell', activeMethod: activeCellMethod}"
           :keyboard-config="{isArrow: true, isDel: true, isEnter: true, isTab: true, isEdit: true}">
           <!-- <vxe-column type="seq" width="60"></vxe-column> -->
-          <vxe-column width="160" field="name" title="募集层一" :edit-render="{autofocus: '.myinput'}">
+          <vxe-column width="140" field="name" title="募集层一" :edit-render="{autofocus: '.myinput'}">
             <template #edit="{ row }">
               <vxe-input v-model="row.name" class="myinput"></vxe-input>
             </template>
@@ -44,7 +43,6 @@
        <vxe-table
           border
           :align="allAlign"
-          style="width:800px"
           size="mini"
           :mouse-config="{selected: true}"
           show-overflow
@@ -69,7 +67,6 @@
         <vxe-table
           border
           :align="allAlign"
-          style="width:800px"
           size="mini"
           :mouse-config="{selected: true}"
           show-overflow
@@ -91,17 +88,18 @@
             </template>
           </vxe-column>
         </vxe-table> 
-        </el-col><el-col span="10">
+        </el-col><el-col :span="10">
     <vxe-table
           align="right"
+          size="mini"
           :data="finalData">
           <vxe-column field="name" title="基金名称" width="160"> </vxe-column>
-          <vxe-column field="marketval" title="市值">
+          <vxe-column field="marketval" title="市值" width="80">
             <template #default="{ row }">
               {{ showMoney(row.marketval) }}
             </template>
             </vxe-column>
-          <!-- <vxe-column field="cash" title="可用资金"> <template #default="{ row }">
+         <!-- --><vxe-column field="cash" title="可用资金"> <template #default="{ row }">
               {{ showMoney(row.cash) }}
             </template>
             </vxe-column>
@@ -120,10 +118,10 @@
           <vxe-column field="process" title="其它"> <template #default="{ row }">
               {{ showMoney(row.process) }}
             </template>
-            </vxe-column> -->
+            </vxe-column> 
 
         </vxe-table>
-
+ 
          <!-- <el-table
           :data="finalData"
           style="width: 100%; margin-bottom: 20px"
@@ -202,47 +200,66 @@
         <div style="display: flex; justify-content: space-between">
           <el-button size="small" @click="addAction()">添加操作</el-button>
         </div>
-        <el-table
+        <vxe-table
           ref="actionTable"
           :data="actionData"
           tooltip-effect="dark"
           :max-height="tmaxh"
           style="width: 100%; margin-top: 20px"
         >
-          <el-table-column
-            label="基金名称"
+          <vxe-column
+            title="基金名称"
             sortable
-            prop="code"
+            field="code"
             show-overflow-tooltip
           >
             <template slot-scope="scope">{{
               showFundName(scope.row.code)
             }}</template>
-          </el-table-column>
-          <el-table-column
-            label="操作"
+          </vxe-column>
+          <vxe-column
+            title="操作"
             sortable
-            prop="stage"
+            field="stage"
             show-overflow-tooltip
           >
-          </el-table-column>
-          <el-table-column
-            label="产品"
+          </vxe-column>
+          <vxe-column
+            title="产品"
             sortable
-            prop="b_code"
+            field="b_code"
             show-overflow-tooltip
           ><template slot-scope="scope">{{
               showFundName(scope.row.b_code)
             }}</template>
-          </el-table-column>
-          <el-table-column
-            label="金额(万)"
+          </vxe-column>
+          <vxe-column
+            title="金额(万)"
             sortable
-            prop="marketval"
+            align="right"
+            field="marketval"
             show-overflow-tooltip
           >
-          </el-table-column> </el-table
-      >
+          </vxe-column>
+          <vxe-column
+            title=""
+            show-overflow-tooltip
+          ><template slot-scope="scope">
+            <el-button  @click.native.prevent="delAction(scope.row,scope.rowIndex)" type="text" size="small"><el-tooltip class="item" effect="dark" content="删除" placement="left-start"><i class="el-icon-delete" style="color:red;"></i></el-tooltip></el-button>
+
+                  </template>
+          </vxe-column>
+          <vxe-column
+            label="操作时间"
+            sortable
+            prop="add_time"
+            show-overflow-tooltip
+          >
+          <template slot-scope="scope">{{
+              $moment(scope.row.add_time).format("YYYY-MM-DD")
+            }}</template>
+          </vxe-column> 
+           </vxe-table>
       </el-tab-pane>
       <el-tab-pane label="资金明细">
         <el-button @click="addAction">添加</el-button>
@@ -499,6 +516,11 @@ export default {
     };
   },
   methods: {
+    delAction(row,idx){
+      this.actionData.splice(idx,1)
+      this.saveAction()
+
+    },
     cellClass1(row){
       let colsize=row.$table.getColumns().length
       if(row.row['name']=='待分配合计' &&row.columnIndex==colsize-1){
@@ -742,6 +764,15 @@ return ''
       }
       return null
     },
+    saveAction(){
+      this.calcFinal();
+          this.$axios({
+      method: 'post',
+      url: "/sys/misc", // 请求地址
+      data: {"code":"fof_action","info":JSON.stringify(this.actionData),"user":this.token}, // 参数
+      responseType: 'json' // 表明返回服务器返回的数据类型
+    }).then((response)=>console.log(response.data))
+    },
     submitForm(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
@@ -749,24 +780,19 @@ return ''
             this.curAction["b_code"] = "CASH";
             this.curAction["b_name"] = "现金";
           }
-          if (this.curAction.id) {
-            for (let i in this.actionData) {
-              if (this.actionData[i]["id"] == curAction.id) {
-                this.actionData[i] = JSON.parse(JSON.stringify(this.curAction));
-                return;
-              }
-            }
-          } else {
-            this.actionData.push(JSON.parse(JSON.stringify(this.curAction)));
-          }
+          // if (this.curAction.id) {
+          //   for (let i in this.actionData) {
+          //     if (this.actionData[i]["id"] == curAction.id) {
+          //       this.actionData[i] = JSON.parse(JSON.stringify(this.curAction));
+          //       return;
+          //     }
+          //   }
+          // } else {
+          this.curAction.add_time=new Date().getTime()
+          this.actionData.push(JSON.parse(JSON.stringify(this.curAction)));
+          // }
           this.actionDiagShow = !this.actionDiagShow;
-          this.calcFinal();
-          this.$axios({
-      method: 'post',
-      url: "/sys/misc", // 请求地址
-      data: {"code":"fof_action","info":JSON.stringify(this.actionData),"user":this.token}, // 参数
-      responseType: 'json' // 表明返回服务器返回的数据类型
-    }).then((response)=>console.log(response.data))
+          this.saveAction()
         } else {
           console.log("error submit!!");
           return false;
