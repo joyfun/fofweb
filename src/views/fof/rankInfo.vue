a9d9e5169dc2d56ac23a40a15120de3a02c7745b
joyfun <joyfun@joyfun.net>
2022-03-21 00:48:18 +0800

    rankinfo improve

--- src/views/fof/rankInfo.vue
+++ src/views/fof/rankInfo.vue
@@ -27,19 +27,20 @@
             <vxe-radio label="2yr" content="2年"></vxe-radio>
           </vxe-radio-group>
           </template>
-        </vxe-toolbar>    
+        </vxe-toolbar>   
        <vxe-table
           border
           ref="rankTable"
+          height="auto"
           :align="allAlign"
           size="mini"
           show-overflow
-          :sort-config="{trigger: 'cell', defaultSort: {field: 'rank', order: 'asc'}, orders: ['desc', 'asc', null]}"
+          :sort-config="{trigger: 'cell', defaultSort: {field: '1yr', order: 'asc'}, orders: ['desc', 'asc', null]}"
           :data="tableData"
         >
-                  <vxe-column type="checkbox" width="30"></vxe-column>
+                  <vxe-column  type="checkbox" width="30"></vxe-column>
           <!-- <vxe-column type="seq" width="60"></vxe-column> -->
-          <vxe-column width="160" field="code" title="产品名称" >
+          <vxe-column  width="200" field="code" align="left" title="产品名称" >
         <template #default="{ row }">
                    <el-button  @click.native.prevent="addCart(row)" type="text" size="small"><el-tooltip class="item" effect="dark" content="添加" placement="left-start"><i class="el-icon-shopping-cart-full" ></i></el-tooltip></el-button>
        <a href="javascript:;" @click="showHis(row)">{{ showFundName(row.code) }}</a>
@@ -47,25 +48,37 @@
 
             </template>
           </vxe-column>
-            <vxe-column width="80" field="date" title="数据日期" >
-          </vxe-column>
-           <vxe-column width="80" field="rank" sortable :title="rankTitle" >
-          </vxe-column>
-          <vxe-column :key="af" sortable v-for="af of ['sharpe', 'calmar', 'sortino', 'dd', 'dd_week', 'win_ratio','yeaily_return', 'volatility']"  :title="af" :field="af"  >
+            <vxe-column  sortable  title="级别"  field="lscore" >
+            <template #default="{ row }">
+              <span>{{ nclassify(row) }}</span>
+            </template>
+          </vxe-column> 
+           <vxe-colgroup  :key="type" :title="type+'排名信息('+rgdict[type]+')'" align="center" v-for=" type of ['hyr','1yr','2yr']">
+               <vxe-column   :field="'rankF_'+type+'_r'" width="60" sortable :title="'rank'" >
+            </vxe-column>
+               <vxe-column :key="dkey" :field="dkey+'_'+type" width="60" sortable :title="dkey"  v-for="dkey in ['std','mean','slope','listrate']">
+            </vxe-column>
+                </vxe-colgroup>
+
+          <vxe-colgroup title="指标数据" align="center">
+                
+         <vxe-column :key="af" width="60" sortable v-for="af of ['sharpe', 'calmar', 'sortino', 'dd', 'dd_week', 'win_ratio','yeaily_return', 'volatility']"  :title="af" :field="af"  >
             <template #default="{ row }">
               <span>{{ row[af] }}</span>
             </template>
           </vxe-column>
-            <vxe-column sortable  title="级别"  field="level" >
-            <template #default="{ row }">
-              <span>{{ classify(row) }}</span>
-            </template>
-          </vxe-column>
+          </vxe-colgroup>
+
+                      <!-- <vxe-column sortable  title="分"  field="lscore" >
+          </vxe-column>  -->
         </vxe-table> 
     </div>
 </template>
 
  <script>
+ const level_dic = {'common': [[-0.03, -0.05, -0.07, -0.1], [0.06, 0.1, 0.13, 0.15]],
+           'cta1': [[-0.13, -0.15, -0.2], [0.15, 0.2, 0.25]]}
+const lvs=["丁","丙","乙","甲"]
 import { mapGetters, mapMutations, mapState } from "vuex";
 import Bus from '@/store/bus.js';
 export default {
@@ -87,7 +100,7 @@ export default {
     },
     range :{
               handler(n){
-                  this.getProducts()
+                  this.getBaseInfo()
             },
     
     },
@@ -112,6 +125,8 @@ export default {
         allAlign:"right",
         tableData:[],
         range:"1yr",
+        baseData:{},
+        rgdict:{},
         date: '',
         type: 'aas',
        cls_gap:{"aas":{"limit":{"dd":[-0.1,0],"volatility":[0,0.15]},"level":{"dd":[-0.07,-0.05,-0.03]}},
@@ -120,7 +135,22 @@ export default {
     };
   },
   methods: {
-
+     nclassify(row){
+       let lmts=level_dic['common'] 
+       let lcnt=4
+       if(this.type=='cta1'){
+         lmts=level_dic['cta1']
+         lcnt=3
+       }
+       let score=0
+       for(let i=0;i<lcnt;i++){
+         if(row['dd']>=lmts[0][i]&&row['volatility']<=lmts[1][i]){
+           score++
+         }
+       }
+      row['lscore']=score
+      return lvs[score-1]
+     },
      classify(row){
        let key=this.type
        row["level"]="丁"
@@ -162,11 +192,38 @@ export default {
           Bus.$emit("showChart",{"cur_code":sels.map(r=>r["code"]).join(','),"diagName":"rankChart"})
 
       },
+    getBaseInfo(){
+     this.$axios
+        .get("/fof/baseinfo", { params: { date: this.date,type:this.type ,range:this.range,code:this.tableData.map(row=>row.code).join()} })
+        .then((response) => {
+          this.baseData =response.data
+          this.tableData.map(row=>{
+            for (let af of ['sharpe', 'calmar', 'sortino', 'dd', 'dd_week', 'win_ratio','yeaily_return', 'volatility']){
+              row[af]=this.baseData[row.code][af]
+            }
+          })
+          this.$refs.rankTable.reloadData(this.tableData)
+        })
+        .catch((error) => {
+          console.log(error);
+        }); 
+    },
     getProducts() {
       this.$axios
-        .get("/fof/rankbyType", { params: { date: this.date,type:this.type ,range:this.range} })
+        .get("/fof/rank2", { params: { date: this.date,type:this.type ,range:this.range} })
         .then((response) => {
-          this.tableData =response.data
+          this.rgdict={}
+          this.tableData =response.data.map(row=>{
+            for(let rg of ['hyr','1yr','2yr']){
+              if(row['rankF_'+rg]){
+                row['rankF_'+rg+'_r']=row['rankF_'+rg].split("-")[0]
+                this.rgdict[rg]=row['rankF_'+rg].split("-")[1]
+              }
+            }
+           return row
+
+          })
+          this.getBaseInfo()
         })
         .catch((error) => {
           console.log(error);
