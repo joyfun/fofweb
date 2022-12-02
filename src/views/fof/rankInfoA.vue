7e5eacebcb092cc01019a4fc3f609ceb374235a2
joyfun <joyfun@joyfun.net>
2022-12-02 19:01:43 +0800

    调整显示

--- src/views/fof/rankInfoA.vue
+++ src/views/fof/rankInfoA.vue
@@ -2,11 +2,11 @@
   <div ref="tableContainer" style="	height : 100%;">
           <vxe-toolbar>
           <template #buttons>
-            <vxe-button @click="clearSelect">取消</vxe-button>
-            <vxe-button @click="showRankHis">排名</vxe-button>
-            <vxe-button @click="showStatHisBatch">指标</vxe-button>
-            <vxe-button @click="compareData">业绩</vxe-button>
-            <vxe-button @click="compareInvest">已投对比</vxe-button>
+            <vxe-button size="mini" @click="clearSelect">取消</vxe-button>
+            <vxe-button size="mini" @click="showRankHis">排名</vxe-button>
+            <vxe-button size="mini" @click="showStatHisBatch">指标</vxe-button>
+            <vxe-button size="mini" @click="compareData">业绩</vxe-button>
+            <vxe-button size="mini" @click="compareInvest">已投对比</vxe-button>
             <el-date-picker style="width:130px"
       v-model="date"
       value-format="yyyyMMdd"
